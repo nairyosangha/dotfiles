af91d507df527409aa23c849e8ba704e9ad2106d
TomDBR <canardo@cock.li>
2020-06-23 22:58:15 +0200

    add flag to allow unstowing easily

--- auto_stow.sh
+++ auto_stow.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+cmd='stow -t'
+while getopts 'd' opt; do
+	case $opt in
+		d) cmd='stow -D -t' ;;
+		*) exit 1
+	esac
+done
+
 for i in */; do
 	if [[ "$i" =~ "media" ]]; then
 		cd ./media
-			for i in */; do echo "media/$i" && stow -t "$HOME" "$i"; done
+		for i in */; do echo "media/$i" && $cmd "$HOME" "$i"; done
 		cd ../
 	else
-		echo "$i" && stow -t $HOME "$i"
+		echo "$i" && $cmd "$HOME" "$i"
 	fi
 done
