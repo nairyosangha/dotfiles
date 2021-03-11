ed43550e1dc36ec2e3ef23905e3db49d42758161
TomDBR <canardo@cock.li>
2021-03-11 19:53:47 +0100

    Show mediainfo preview for more filetypes

--- CLI/.config/lf/pv.sh
+++ CLI/.config/lf/pv.sh
@@ -4,6 +4,6 @@ case "$1" in
 	*.tar*) tar tf "$1";;
 	*.zip) unzip -l "$1";;
 	*.pdf) pdftotext "$1" -;;
-	*.gif|*.webm|*.png|*.jpg|*.flac|*.opus|*.mkv|*.mp3) mediainfo "$1";;
+	*.gif|*.webm|*.png|*.jpg|*.flac|*.opus|*.mkv|*.mp3|*.mp4) mediainfo "$1";;
 	*) highlight -O ansi "$1" || cat "$1";;
 esac
